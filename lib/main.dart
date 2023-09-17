fa189b8219644442306c1b622b5b0439349163bb
mido-ahmed <220190077@fa-hists.edu.eg>
2023-09-17 15:27:03 +0300

    delete extra url in main file

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:todo_list_app/layout/home_layout.dart';
 import 'package:bloc/bloc.dart';
 import 'package:todo_list_app/shared/bloc_observer/bloc_observer.dart';
-import 'modules/counter_screen/counter.dart';
 
 void main() {
   Bloc.observer = MyBlocObserver();
